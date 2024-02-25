1ff46b59fdf519e30a3466b95c379bcf81e3f4f3
justinbl <me289673098@gmail.com>
2024-02-25 19:59:33 +1000

    add gitignore

---
+++ README.md
@@ -0,0 +1,22 @@
+# pyCRISM
+A python implementation of CAT to process CRISM data
+
+## Installation
+
+The code requires GDAL and you can install it with Anaconda
+```bash
+ conda create -n pycrism
+ conda actiavte pycrism
+ conda install -c conda-forge gdal
+```
+
+To use the package from any path, install it in your environment with the following command from the project root:
+```bash
+pip install -e .
+```
+## Usage
+
+
+
+## License
+This package is released under a MIT open source license. See LICENSE for more details.
---
+++ setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open('requirements.txt', 'r') as f:
+    requirements = f.read().strip('\n').split('\n')
+
+package_data = {
+    '': ['aux_files/*','aux_files/WA/*','aux_files/VS_ADR/*'],
+    }
+setuptools.setup(
+    name="pyCRISM",
+    version="1.0.0",
+    author='Binlong Ye',
+    author_email='binlongy@connect.hku.hk',
+    description='A python implementation of CAT to process CRISM data',
+    long_description_content_type='text/markdown',
+    url='https://github.com/justinbl/pyCRISM/',
+    project_urls={
+        'Source': 'https://github.com/justinbl/pyCRISM/',
+    },
+    packages=setuptools.find_packages(),
+    package_data=package_data,
+    py_modules=["pyCRISM"],
+    python_requires='>=3.8',
+    setup_requires=['wheel'],
+    install_requires=requirements,
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Astronomy'
+        ]
+    )
